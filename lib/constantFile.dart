7fe7a623e0fb7691a82b6f481ef2144677c94707
MuhammadUsmanIbraheem <usmanibraheem626@gmail.com>
2020-11-26 03:10:20 +0500

    Done lecture 69 : complete UI of 2nd screen

--- lib/constantFile.dart
+++ lib/constantFile.dart
@@ -14,3 +14,15 @@ const kLargeButtonstyle = TextStyle(
   fontSize: 20.0,
   fontWeight: FontWeight.bold,
 );
+const kTitleStyleS2 = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+const kResultText = TextStyle(
+  color: Colors.green,
+  fontSize: 20.0,
+);
+const kBmiTextStyle = TextStyle(
+  fontSize: 80.0,
+);
+const kbodyTextStyle = TextStyle();
--- lib/resultFile.dart
+++ lib/resultFile.dart
@@ -1,13 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:new_bmi_calculator/constantFile.dart';
+import 'package:new_bmi_calculator/ContainerFile.dart';
+import 'input_page.dart';
 
 class ResultScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI Result'),
-      ),
-      body: Text('body data'),
-    );
+        appBar: AppBar(
+          title: Text('BMI Result'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: Container(
+                child: Center(
+                  child: Text(
+                    'Your Result',
+                    style: kTitleStyleS2,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 5,
+              child: RepeatContainercode(
+                colors: activeColor,
+                cardWidget: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Normal',
+                      style: kResultText,
+                    ),
+                    Text(
+                      '18.3',
+                      style: kBmiTextStyle,
+                    ),
+                    Text(
+                      'BMI is low You should have to work more',
+                      textAlign: TextAlign.center,
+                      style: kbodyTextStyle,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => InputPage()));
+                },
+                child: Container(
+                  child: Center(
+                    child: Text(
+                      'ReCalculate',
+                      style: kLargeButtonstyle,
+                    ),
+                  ),
+                  color: Color(0xFFEB1555),
+                  margin: EdgeInsets.only(top: 10.0),
+                  width: double.infinity,
+                  height: 50.0,
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
